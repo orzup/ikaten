ded665dfb4605c3ef4c0be210ac47d1f131f36d3
Ryuji Mori <rmg2333@gmail.com>
2016-03-14 21:21:02 +0900

    createBattleViewから値を取得して送信できるように修正

--- ikaten/Classes/Controllers/CreateButtleViewController.swift
+++ ikaten/Classes/Controllers/CreateButtleViewController.swift
@@ -2,7 +2,13 @@ import UIKit
 import SVProgressHUD
 
 class CreateButtleViewController: UIViewController {
-    var params: Dictionary<String, AnyObject>!
+    var battle: Battle?
+
+    @IBOutlet weak var mapSelector: UISegmentedControl!
+    @IBOutlet weak var resultSelector: UISegmentedControl!
+    @IBOutlet weak var killsTextField: UITextField!
+    @IBOutlet weak var deathTextField: UITextField!
+    @IBOutlet weak var rankExpChangeTextField: UITextField!
 
     @IBAction func touchUpInsideNextBattleButton(sender: AnyObject) {
         createButtle()
@@ -10,9 +16,17 @@ class CreateButtleViewController: UIViewController {
 
     private func createButtle() {
         SVProgressHUD.show()
-        addParams()
+        // TODO: マップの取得
+        battle!.setResult([
+            "map"          : "",
+            "result"       : result(),
+            "kill"         : Int(killsTextField.text!)!,
+            "death"        : Int(deathTextField.text!)!,
+            "rankExpChange": Int(rankExpChangeTextField.text!)!,
+            "knockOut"     : false
+            ])
 
-        StatInk().createButtle(Battle(data: params),
+        StatInk().createButtle(battle!,
             onSuccess: { (response) -> Void in
                 SVProgressHUD.showSuccessWithStatus("キロク OK!")
             }, onFailure: { (errorResponse) -> Void in
@@ -20,12 +34,17 @@ class CreateButtleViewController: UIViewController {
         })
     }
 
-    private func addParams() {
-        params["rankAfter"]    = ""
-        params["rankExpAfter"] = 0
-        params["result"]       = ""
-        params["kill"]         = 0
-        params["death"]        = 0
-        params["knockOut"]     = false
+    private func result() -> String {
+        var result = ""
+        switch resultSelector.selectedSegmentIndex {
+        case 0:
+            result = "win"
+        case 1:
+            result = "lose"
+        default:
+            break
+        }
+        return result
     }
 }
+
--- ikaten/Classes/Models/Battle.swift
+++ ikaten/Classes/Models/Battle.swift
@@ -1,46 +1,68 @@
 class Battle {
     let lobby: String
     let rule: String
-    let map: String
     let weapon: String
     let rank: String
     let rankExp: Int
-    var rankAfter: String
-    var rankExpAfter: Int
-    var result: String
-    var kill: Int
-    var death: Int
-    var knockOut: Bool
+    var map: String?
+    var rankAfter: String?
+    var rankExpAfter: Int?
+    var result: String?
+    var kill: Int?
+    var death: Int?
+    var knockOut: Bool?
 
     init(let data: Dictionary<String, AnyObject>) {
         lobby        = data["lobby"] as! String
         rule         = data["rule"] as! String
-        map          = data["map"] as! String
         weapon       = data["weapon"] as! String
         rank         = data["rank"] as! String
         rankExp      = data["rank_exp"] as! Int
-        rankAfter    = data["rankAfter"] as! String
-        rankExpAfter = data["rankExpAfter"] as! Int
-        result       = data["result"] as! String
-        kill         = data["kill"] as! Int
-        death        = data["death"] as! Int
-        knockOut     = data["knockOut"] as! Bool
+    }
+
+    func setResult(data: Dictionary<String, AnyObject>) {
+        var ranks = ["c-", "c", "c+", "b-", "b", "b+", "a-", "a", "a+", "s", "s+"];
+        let rankExpChange = data["rankExpChange"] as! Int
+        if  data["result"] as! String == "win" {
+            rankExpAfter = rankExp + rankExpChange
+            if rank != "s+" && rankExpAfter >= 100 {
+                rankExpAfter = 30
+                rankAfter = ranks[ranks.indexOf(rank)! + 1];
+            } else {
+                rankAfter = rank;
+            }
+        }
+        else {
+            rankExpAfter = rankExp - rankExpChange
+            if rank != "c-" && rankExpAfter < 0 {
+                rankExpAfter = 70
+                rankAfter = ranks[ranks.indexOf(rank)! - 1];
+            } else {
+                rankAfter = rank;
+            }
+        }
+
+        map          = data["map"] as? String
+        result       = data["result"] as? String
+        kill         = data["kill"] as? Int
+        death        = data["death"] as? Int
+        knockOut     = data["knockOut"] as? Bool
     }
 
     func decode() -> Dictionary<String, AnyObject> {
-        let knockOutString = knockOut ? "yes" : "no"
+        let knockOutString = knockOut! ? "yes" : "no"
         return [
             "lobby":          lobby,
             "rule":           rule,
-            "map":            map,
+            "map":            map!,
             "weapon":         weapon,
             "rank":           rank,
             "rank_exp":       rankExp,
-            "rank_after":     rankAfter,
-            "rank_exp_after": rankExpAfter,
-            "result":         result,
-            "kill":           kill,
-            "death":          death,
+            "rank_after":     rankAfter!,
+            "rank_exp_after": rankExpAfter!,
+            "result":         result!,
+            "kill":           kill!,
+            "death":          death!,
             "knock_out":      knockOutString
        ]
     }
